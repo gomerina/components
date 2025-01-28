554b556967a0754f59eecdeaa797c0102d0602d0
gomerina <KuzovkovSer52reg@yandex.ru>
2025-01-28 11:38:19 +0300

    add UI

--- gulpfile.js
+++ gulpfile.js
@@ -37,126 +37,13 @@ function browsersync() {
     ghostMode: false,
   })
 }
-
-function buildPug() {
-  return src(['app/**/*.pug'])
-    .pipe(pug({
-      pretty: true
-    }))
-    .pipe(formatHTML({
-      indent_size: 4,
-      indent_with_tabs: true,
-    }))
-    .pipe(dest('./dist/'))
-    .pipe(browserSync.stream())
-}
-
-function styles() {
-  return src(['app/scss/**/style.scss'])
-    .pipe(sassglob())
-    .pipe(sass({
-      'include css': true,
-      outputStyle: 'expanded'
-    }))
-    .pipe(postCss([
-      autoprefixer({
-        grid: true,
-        overrideBrowserslist: ['last 3 versions'],
-        cascade: false
-      }),
-      mediaQueries({
-        sort: 'desktop-first'
-      })
-    ]))
-    .pipe(dest('./dist/css/'))
-    .pipe(postCss([
-      cssnano({ preset: ['default', { discardComments: { removeAll: true } }] })
-    ]))
-    .pipe(concat('style.min.css'))
-    .pipe(dest('./dist/css/'))
-    .pipe(browserSync.stream())
-}
-function componentsStyles() {
-  return src(['app/includes/**/style.scss'])
-    .pipe(sassglob())
-    .pipe(sass({
-      'include css': true,
-      outputStyle: 'expanded'
-    }))
-    .pipe(postCss([
-      autoprefixer({
-        grid: true,
-        overrideBrowserslist: ['last 3 versions'],
-        cascade: false
-      }),
-    ]))
-    .pipe(dest('./dist/includes/'))
-    .pipe(postCss([
-      cssnano({ preset: ['default', { discardComments: { removeAll: true } }] })
-    ]))
-    .pipe(browserSync.stream())
-}
-function scripts() {
-  return src(['app/js/*.js'])
-    .pipe(named())
-    .pipe(dest('./dist/js/'))
-    .pipe(browserSync.stream())
-}
-
-function images() {
-  return src(['app/img/**/*'])
-    .pipe(changed('./dist/img/'))
-    .pipe(imagemin([
-      gifsicle({ interlaced: true }),
-      mozjpeg({
-        progressive: true,
-        quality: 80
-      }),
-      pngQuant(),
-      svgo({
-        plugins: [
-          {
-            name: 'removeViewBox',
-            active: true
-          },
-          {
-            name: 'cleanupIDs',
-            active: false
-          }
-        ]
-      })
-    ], {
-      verbose: true
-    }))
-    .pipe(dest('./dist/img/'))
-    .pipe(webp())
-    .pipe(dest('./dist/img/webp'))
-    .pipe(browserSync.stream())
-}
-
-function fonts() {
-  return src(['app/fonts/**/*'])
-    .pipe(dest('./dist/css/fonts/'))
-    .pipe(browserSync.stream())
-}
-
-async function cleandist() {
-  del('./dist/**/*', { force: true })
-}
-
 function startwatch() {
-  gulpWatch(['./app/**/*.pug'], { usePolling: true }, buildPug)
-  gulpWatch(['./app/scss/**/*.scss', './app/includes/**/*.scss'], { usePolling: true }, styles)
-  gulpWatch(['./app/includes/**/*.scss'], { usePolling: true }, componentsStyles)
-  gulpWatch(['./app/js/**/*.js'], { usePolling: true }, scripts)
-  gulpWatch(['./app/img/**/*'], { usePolling: true }, images)
-  gulpWatch(['./app/fonts/**/*'], { usePolling: true }, fonts)
   gulpWatch(['./dist/**/*.*'], { usePolling: true }).on('change', browserSync.reload)
 }
 
-const build = series(cleandist, parallel(images, scripts, buildPug, styles, componentsStyles, fonts))
-const watch = series(parallel(images, scripts, buildPug, styles, componentsStyles, fonts), parallel(browsersync, startwatch))
+
+const watch = series(parallel(browsersync, startwatch))
 
 
-export { build, watch }
+export { watch }
 export default watch;
